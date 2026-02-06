c683cd7df11b04eea370f714996892ad74f3bccd
Stephen Lyons <slysven@virginmedia.com>
2026-02-06 17:45:29 +0000

    Infrastructure: update en_US plural translations 2026/02/06
    Signed-off-by: Stephen Lyons <slysven@virginmedia.com>

--- translations/translated/mudlet_en_US.ts
+++ translations/translated/mudlet_en_US.ts
@@ -15,20 +15,13 @@
 <context>
     <name>MapInfoContributorManager</name>
     <message numerus="yes">
-        <source>Room%1ID:%1%2 Position%1on%1Map: (%3,%4,%5) ‑%1center of %n selected rooms</source>
-        <extracomment>This text uses non-breaking spaces (as &apos;%1&apos;s, as Qt Creator cannot handle them literally in raw strings) and a non-breaking hyphen which are used to prevent the line being split at some places it might otherwise be; when translating please consider at which points the text may be divided to fit onto more than one line. This text is for when TWO or MORE rooms are selected; %1 is the room number for which %2-%4 are the x,y and z coordinates of the room nearest the middle of the selection. This room has the yellow cross-hairs. %n is the count of rooms selected and will ALWAYS be greater than 1 in this situation. It is provided so that non-English translations can select required plural forms as needed.</extracomment>
-        <translation type="vanished">
-            <numerusform>{unused} Room%1ID:%1%2 Position%1on%1Map: (%3,%4,%5) ‑%1center of %n selected rooms</numerusform>
-            <numerusform>Room%1ID:%1%2 Position%1on%1Map: (%3,%4,%5) ‑%1center of %n selected rooms</numerusform>
-        </translation>
-    </message>
-    <message numerus="yes">
         <location filename="../../src/mapInfoContributorManager.cpp" line="220"/>
         <source>Center of %n selected rooms</source>
         <extracomment>This description is shown when MORE THAN ONE room is selected.</extracomment>
-        <translation type="unfinished">
-            <numerusform></numerusform>
-            <numerusform></numerusform>
+        <translatorcomment>The singular form is not used.</translatorcomment>
+        <translation>
+            <numerusform>Center of %n selected room</numerusform>
+            <numerusform>Center of %n selected rooms</numerusform>
         </translation>
     </message>
 </context>
@@ -118,18 +111,20 @@ be in these areas...</numerusform>
         <source>[ ALERT ] - Socket got disconnected, for %n reason(s):
 %1</source>
         <extracomment>This message is used when we have been trying to connect or * we were connected securely, but the connection has been lost. * It is possible with a secure connection that there is MORE * than one error message to show, but for English or other * locales where the singular case (%n==1) is distinct it would * be perfectly feasible to replace &quot;for %n reason(s)&quot; with * &quot;because&quot; for that number (1) of errors - however the text * should then be repeated in the corresponding situation for * an &quot;open&quot; connection which is different in that it only ever * has one &quot;reason&quot; to report.</extracomment>
-        <translation type="unfinished">
-            <numerusform></numerusform>
-            <numerusform></numerusform>
+        <translation>
+            <numerusform>[ ALERT ] - Socket got disconnected, for %n reason:
+%1</numerusform>
+            <numerusform>[ ALERT ] - Socket got disconnected, for %n reasons:
+%1</numerusform>
         </translation>
     </message>
     <message numerus="yes">
         <location filename="../../src/ctelnet.cpp" line="954"/>
         <source>The %n IP address(es) of %1 has/have been found. It/They are:</source>
         <extracomment>This text is used in the (expected) case when the user has provided * a URL (%1) for the Game Server rather than (unusually) an IP address. * After a DNS lookup we have found at least one but possibly more (%n) * IP addresses, which will be listed (one per line) immediately * afterwards.</extracomment>
-        <translation type="unfinished">
-            <numerusform></numerusform>
-            <numerusform></numerusform>
+        <translation>
+            <numerusform>The %n IP address of %1 has been found. It is:</numerusform>
+            <numerusform>The %n IP addresses of %1 have been found. They are:</numerusform>
         </translation>
     </message>
 </context>
@@ -148,38 +143,32 @@ be in these areas...</numerusform>
 <context>
     <name>dlgPackageManager</name>
     <message numerus="yes">
-        <source>Remove %n package(s)</source>
-        <extracomment>Message on button in package manager to remove one or more (%n is the count of) selected package(s).</extracomment>
-        <translation type="vanished">
-            <numerusform>Remove %n package</numerusform>
-            <numerusform>Remove %n packages</numerusform>
-        </translation>
-    </message>
-    <message numerus="yes">
         <location filename="../../src/dlgPackageManager.cpp" line="799"/>
         <source>Update (%n)</source>
         <extracomment>Message on button in package manager to update multiple (%n is the count of) selected packages.</extracomment>
-        <translation type="unfinished">
-            <numerusform></numerusform>
-            <numerusform></numerusform>
+        <translation>
+            <numerusform>Update (%n)</numerusform>
+            <numerusform>Update (%n)</numerusform>
         </translation>
     </message>
     <message numerus="yes">
         <location filename="../../src/dlgPackageManager.cpp" line="809"/>
         <source>Install (%n)</source>
         <extracomment>Message on button in package manager to install multiple (%n is the count of) selected packages.</extracomment>
-        <translation type="unfinished">
-            <numerusform></numerusform>
-            <numerusform></numerusform>
+        <translatorcomment>The code execution path means this will not be used when a single package is to be installed...</translatorcomment>
+        <translation>
+            <numerusform>Install (%n)</numerusform>
+            <numerusform>Install (%n)</numerusform>
         </translation>
     </message>
     <message numerus="yes">
         <location filename="../../src/dlgPackageManager.cpp" line="834"/>
         <source>Remove (%n)</source>
         <extracomment>Message on button in package manager to remove multiple (%n is the count of) selected packages.</extracomment>
-        <translation type="unfinished">
-            <numerusform></numerusform>
-            <numerusform></numerusform>
+        <translatorcomment>The code execution path means this will not be used when a single package is to be installed...</translatorcomment>
+        <translation>
+            <numerusform>Remove (%n)</numerusform>
+            <numerusform>Remove (%n)</numerusform>
         </translation>
     </message>
 </context>
@@ -212,9 +201,10 @@ be in these areas...</numerusform>
         <source>Lock room(s), so it/they will never be used for speedwalking</source>
         <comment>This text will be shown at a checkbox, where you can set/unset a number of room&apos;s lock.</comment>
         <extracomment>room properties dialog, setting lock status</extracomment>
+        <translatorcomment>%n is missing from Engineering English source text</translatorcomment>
         <translation>
-            <numerusform>Lock room, so it will never be used for speedwalking</numerusform>
-            <numerusform>Lock rooms, so they will never be used for speedwalking</numerusform>
+            <numerusform>Lock %n room, so it will never be used for speedwalking</numerusform>
+            <numerusform>Lock %n rooms, so they will never be used for speedwalking</numerusform>
         </translation>
     </message>
     <message numerus="yes">
@@ -222,9 +212,10 @@ be in these areas...</numerusform>
         <source>Hide room(s) from the map display</source>
         <comment>This text will be shown at a checkbox, where you can set/unset a number of room&apos;s hidden status.</comment>
         <extracomment>room properties dialog, setting hidden status</extracomment>
-        <translation type="unfinished">
-            <numerusform></numerusform>
-            <numerusform></numerusform>
+        <translatorcomment>%n is missing from Engineering English source text</translatorcomment>
+        <translation>
+            <numerusform>Hide %n room from the map display</numerusform>
+            <numerusform>Hide %n rooms from the map display</numerusform>
         </translation>
     </message>
     <message numerus="yes">
@@ -280,31 +271,6 @@ be in these areas...</numerusform>
  • an existing symbol from the list,
  • enter one or more characters to set a new symbol,
  • clear to unset.</numerusform>
-        </translation>
-    </message>
-    <message numerus="yes">
-        <source>Type one or more graphemes (&quot;visible characters&quot;) to use as a symbol for all of the %n selected room(s), or enter a space to clear the symbol:</source>
-        <comment>%n is the total number of rooms involved.</comment>
-        <translation type="vanished">
-            <numerusform>Type one or more graphemes (&quot;visible characters&quot;) to use as a symbol for the %n selected room, or enter a space to clear the symbol:</numerusform>
-            <numerusform>Type one or more graphemes (&quot;visible characters&quot;) to use as a symbol for all of the %n selected rooms, or enter a space to clear the symbol:</numerusform>
-        </translation>
-    </message>
-    <message numerus="yes">
-        <source>To change the symbol for all of the %n selected room(s), please choose:
- • an existing symbol from the list below (sorted by most commonly used first)
- • enter one or more graphemes (&quot;visible characters&quot;) as a new symbol
- • enter a space to clear any existing symbols</source>
-        <comment>This is for when applying a new room symbol to one or more rooms and some have different symbols or no symbol at present. %n is the total number of rooms involved.</comment>
-        <translation type="vanished">
-            <numerusform>To change the symbol for the %n selected rooms, please choose:
- • an existing symbol from the list below (sorted by most commonly used first)
- • enter one or more graphemes (&quot;visible characters&quot;) as a new symbol
- • enter a space to clear any existing symbols</numerusform>
-            <numerusform>To change the symbol for all of the %n selected rooms, please choose:
- • an existing symbol from the list below (sorted by most commonly used first)
- • enter one or more graphemes (&quot;visible characters&quot;) as a new symbol
- • enter a space to clear any existing symbols</numerusform>
         </translation>
     </message>
 </context>
